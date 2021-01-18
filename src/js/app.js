f1383d76aa0a9768f53bcb350f2d76978be56dda
Vadym Holovin <holyvad@gmail.com>
2021-01-18 19:16:21 +0200

    Styled settings page

--- src/js/app.js
+++ src/js/app.js
@@ -343,6 +343,72 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 /**
+ * Drag and Drop
+ */
+document.addEventListener('DOMContentLoaded', function(event) {
+  var dragSrcEl = null;
+
+  function handleDragStart(e) {
+    this.style.opacity = '0.4';
+
+    dragSrcEl = this;
+
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  }
+
+  function handleDragOver(e) {
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+
+    e.dataTransfer.dropEffect = 'move';
+
+    return false;
+  }
+
+  function handleDragEnter(e) {
+    this.classList.add('over');
+  }
+
+  function handleDragLeave(e) {
+    this.classList.remove('over');
+  }
+
+  function handleDrop(e) {
+    if (e.stopPropagation) {
+      e.stopPropagation();
+    }
+
+    if (dragSrcEl != this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+
+    return false;
+  }
+
+  function handleDragEnd(e) {
+    this.style.opacity = '1';
+
+    items.forEach(function (item) {
+      item.classList.remove('over');
+    });
+  }
+
+  let items = document.querySelectorAll('.js-draggable');
+  items.forEach(function(item) {
+    item.addEventListener('dragstart', handleDragStart, false);
+    item.addEventListener('dragenter', handleDragEnter, false);
+    item.addEventListener('dragover', handleDragOver, false);
+    item.addEventListener('dragleave', handleDragLeave, false);
+    item.addEventListener('drop', handleDrop, false);
+    item.addEventListener('dragend', handleDragEnd, false);
+  });
+});
+
+
+/**
  * Tabs
  */
 jQuery(function ($) {
@@ -400,8 +466,21 @@ jQuery(function ($) {
  * Form
  */
 jQuery(function ($) {
+  // Masks
   $('.js-phone-mask').inputmask({"mask": "+7 (999) 999-99-99"});
   $('.js-date-mask').inputmask("99.99.9999", { "placeholder": "дд.мм.гггг" });
+
+
+  // Show password
+  $(document).on('click', '.js-show-pass', function (e) {
+    e.preventDefault();
+    let $toggler = $(this);
+    let $input = $(this).parent().find('input');
+    let type = ($input.attr('type') === 'password') ? 'text' : 'password';
+
+    $toggler.toggleClass('is-active');
+    $input.attr('type', type);
+  });
 });
 
 
@@ -416,5 +495,7 @@ jQuery(function ($) {
     $btn.addClass('is-active');
   });
 });
+
+
 
 
